e2cc29412da42e299d81a3b8f1628be248b47150
fesoking <132924721+fesoking@users.noreply.github.com>
2023-11-13 11:51:03 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,57 +2,112 @@
 let questions = [
   {
     numb: 1,
-    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
-    answer: "Marie Curie",
+    question: "Qual o menor país do mundo?",
+    answer: "Vaticano",
     options: [
-      "Albert Einstein",
-      "Marie Curie",
-      "Stephen Hawking",
-      "Linus Pauling"
+      "Liechtenstein",
+      "Vaticano",
+      "Lesoto",
+      "Luxemburgo"
     ]
   },
   {
     numb: 2,
-    question: "Qual o país mais novo do mundo?",
-    answer: "Sudão do Sul",
+    question: "Qual time de futebol é o maior campeão do campeonato brasileiro?",
+    answer: "Palmeiras",
     options: [
-      "Timor Leste",
-      "Montenegro",
-      "Sudão do Sul",
-      "Kosovo"
+      "Corinthians",
+      "Flamengo",
+      "São Paulo",
+      "Palmeiras"
     ]
   },
   {
     numb: 3,
-    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
-    answer: "Solstício de verão",
+    question: "Qual cientista ficou conhecido com propor a teoria da Relatividade?",
+    answer: "Albert Einstein",
     options: [
-      "Equinócio de outono",
-      "Solstício de inverno",
-      "Solstício de verão",
-      "Solstício de outono"
+      "Albert Einstein",
+      "Isaac Newton",
+      "Marie Curie",
+      "Galileu Galilei"
     ]
   },
   {
     numb: 4,
-    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
-    answer: "12",
+    question: "Em que ano a cidade de Brasília foi inaugurada?",
+    answer: "1960",
     options: [
-      "12",
-      "11",
-      "14",
-      "13"
+      "1955",
+      "1964",
+      "1960",
+      "1972"
     ]
   },
   {
     numb: 5,
-    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
-    answer: "Vênus e Urano",
+    question: "Qual cantor ficou mundialmente famoso pela música Despacito?",
+    answer: "Luis Fonsi",
     options: [
-      "Saturno e Júpiter",
-      "Vênus e Urano",
-      "Terra e Marte",
-      "Mercúrio e Saturno"
+      "Kondzilla",
+      "Luis Fonsi",
+      "Daddy Yankee",
+      "J Balvin"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Qual é a estrutura fundamental de todos os seres vivos?",
+    answer: "célula",
+    options: [
+      "célula",
+      "átomo",
+      "órgão",
+      "tecido"
+    ]
+  },
+   {
+    numb: 7,
+    question: "Em que cidade está localizada o grande centro cinematográfico de Hollywood?",
+    answer: "Los Angeles",
+    options: [
+      "Paris",
+      "Londres",
+      "Nova York",
+      "Los Angeles"
+    ]
+  },
+   {
+    numb: 8,
+    question: "Qual é o nome do maior prêmio da indústria musical?",
+    answer: "Grammy",
+    options: [
+      "Grammy Awards",
+      "Oscar",
+      "Globo de Ouro",
+      "MTV Music Awards"
+    ]
+  },
+   {
+    numb: 9,
+    question: "Qual o nome da teoria científica, que propõe que a Terra está no centro do Universo?",
+    answer: "Geocentrismo",
+    options: [
+      "Heliocentrismo",
+      "Empirismo",
+      "Geocentrismo",
+      "Centrismo"
+    ]
+  },
+   {
+    numb: 10,
+    question: "Qual país extinto ficou conhecido pelo seu famoso sistema comunista?",
+    answer: "União Soviética",
+    options: [
+      "Tchecoslováquia",
+      "União Soviética",
+      "Alemanha Ocidental",
+      "Iugoslávia"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
